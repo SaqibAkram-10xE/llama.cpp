bd661a3c56005c672a21462a56b52eca3827dc79
SaqibAkram-10xE <saqib.akram@10xengineers.ai>
2026-03-17 02:59:12 +0500

    Update ggml-et.cpp

--- ggml/src/ggml-et/ggml-et.cpp
+++ ggml/src/ggml-et/ggml-et.cpp
@@ -508,10 +508,10 @@ static enum ggml_status ggml_backend_et_graph_compute(ggml_backend_t backend, gg
     // }
     
     
-    // ggml_et_op_mul(dev_ctx, cgraph);
+    ggml_et_op_mul(dev_ctx, cgraph);
 
     // printf("\n\n\n\n\n\n");
-    for (int i = 0; i < cgraph->n_nodes; i++) {
+    /*for (int i = 0; i < cgraph->n_nodes; i++) {
         ggml_tensor * node = cgraph->nodes[i];
         // printf("Node# %d, Node->Op: %d,  %s\n", i, node->op, ggml_op_name(node->op));
 
@@ -557,6 +557,7 @@ static enum ggml_status ggml_backend_et_graph_compute(ggml_backend_t backend, gg
                 break;
 
             case GGML_OP_RMS_NORM:
+                printf("GGML_OP_RMS_NORM CALLED\n");
                 ggml_et_op_rms_norm(dev_ctx, node);
                 break;
 
@@ -592,7 +593,7 @@ static enum ggml_status ggml_backend_et_graph_compute(ggml_backend_t backend, gg
                 return GGML_STATUS_FAILED;
         }
          
-    }
+    }*/
     
 
     return GGML_STATUS_SUCCESS;
