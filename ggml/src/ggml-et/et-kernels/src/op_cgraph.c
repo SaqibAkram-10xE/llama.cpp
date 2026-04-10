aef5fb9e90a783cce0df27b3f64eee55a1d85658
SaqibAkram-10xE <saqib.akram@10xengineers.ai>
2026-04-10 14:42:17 +0500

    Adding Graph to op_cgraph.c

--- ggml/src/ggml-et/et-kernels/src/op_cgraph.c
+++ ggml/src/ggml-et/et-kernels/src/op_cgraph.c
@@ -462,22 +462,163 @@ int entry_point(struct ggml_cgraph_et* cg, void* env) {
     // device_barrier(32);
 
 
-    /*for (int i = 0; i < n_nodes; i++) {
-        const int op = node_op[i];
-        
-        if (op == GGML_OP_NONE) {
-            continue;
-        }
+    for (int i = 0; i < n_nodes; i++)
+    {
+        const int node_op_val = node_op[i];
+        if (node_op_val == GGML_OP_NONE) continue;
+        // device_barrier(32);
+  
+        /*switch (node_op_val) {
+            case GGML_OP_SQR:
+                ggml_et_op_sqr(dev_ctx, node);
+                break;
 
-        // Checks if current node is RMS_NORM and next is MUL to fuse them
-        if (ggml_et_can_fuse(cg, i, (enum ggml_op[]){ GGML_OP_RMS_NORM, GGML_OP_MUL }, 2)) {
-            ggml_et_op_rms_norm_mul(env, &node_meta[i], &node_meta[i + 1]);
-            i++; // Skip the next node as it's now fused
-            device_barrier(32);
-            continue;
-        }
+            case GGML_OP_UNARY:
+                ggml_et_op_unary(dev_ctx, node);
+                break;
 
-        switch (op) {
+            case GGML_OP_SUM_ROWS:
+                ggml_et_op_sum_rows(dev_ctx, node);
+                break;
+
+            case GGML_OP_SUB:
+            case GGML_OP_ADD:
+            case GGML_OP_MUL:
+                ggml_et_op_mul(dev_ctx, node);
+                break;
+
+            case GGML_OP_CUMSUM:
+                ggml_et_op_cumsum(dev_ctx, node);
+                break;
+
+            case GGML_OP_MUL_MAT:
+                ggml_et_op_mul_mat(dev_ctx, node);
+                break;
+
+            case GGML_OP_MUL_MAT_ID:
+                ggml_et_op_mul_mat_id(dev_ctx, node);
+                break;
+
+            case GGML_OP_ROPE:
+                ggml_et_op_rope(dev_ctx, node);
+                break;
+
+            case GGML_OP_RMS_NORM:
+                ggml_et_op_rms_norm(dev_ctx, node);
+                break;
+
+            case GGML_OP_NORM:
+                ggml_et_op_norm(dev_ctx, node);
+                break;
+
+            case GGML_OP_L2_NORM:
+                ggml_et_op_l2_norm(dev_ctx, node);
+                break;
+
+            case GGML_OP_GROUP_NORM:
+                ggml_et_op_group_norm(dev_ctx, node);
+                break;
+
+            case GGML_OP_SCALE:
+                ggml_et_op_scale(dev_ctx, node);
+                break;
+
+            case GGML_OP_GLU:
+                ggml_et_op_glu(dev_ctx, node);
+                break;
+
+            case GGML_OP_SOFT_MAX:
+                ggml_et_op_softmax(dev_ctx, node);
+                break;
+
+            case GGML_OP_IM2COL:
+                ggml_et_op_im2col(dev_ctx, node);
+                break;
+
+            case GGML_OP_FLASH_ATTN_EXT:
+                ggml_et_op_flash_attn_ext(dev_ctx, node);
+                break;
+
+            case GGML_OP_GET_ROWS:
+                ggml_et_op_get_rows(dev_ctx, node);
+                break;
+
+            case GGML_OP_CONT:
+                ggml_et_op_cont(dev_ctx, node);
+                break;
+
+            case GGML_OP_CPY:
+                ggml_et_op_cpy(dev_ctx, node);
+                break;
+
+            case GGML_OP_CONCAT:
+                ggml_et_op_concat(dev_ctx, node);
+                break;
+
+            case GGML_OP_REPEAT:
+                ggml_et_op_repeat(dev_ctx, node);
+                break;
+
+            case GGML_OP_SSM_CONV:
+                ggml_et_op_ssm_conv(dev_ctx, node);
+                break;
+
+            case GGML_OP_SSM_SCAN:
+                ggml_et_op_ssm_scan(dev_ctx, node);
+                break;
+
+            case GGML_OP_PAD:
+                ggml_et_op_pad(dev_ctx, node);
+                break;
+
+            case GGML_OP_SET_ROWS:
+                ggml_et_op_set_rows(dev_ctx, node);
+                break;
+
+            case GGML_OP_FILL:
+                ggml_et_op_fill(dev_ctx, node);
+                break;
+
+            case GGML_OP_DIAG:
+                ggml_et_op_diag(dev_ctx, node);
+                break;
+
+            case GGML_OP_TRI:
+                ggml_et_op_tri(dev_ctx, node);
+                break;
+
+            case GGML_OP_SOLVE_TRI:
+                ggml_et_op_solve_tri(dev_ctx, node);
+                break;
+
+            case GGML_OP_SET:
+                ggml_et_op_set(dev_ctx, node);
+                break;
+
+            case GGML_OP_RWKV_WKV6:
+                ggml_et_op_rwkv_wkv6(dev_ctx, node);
+                break;
+
+            case GGML_OP_RWKV_WKV7:
+                ggml_et_op_rwkv_wkv7(dev_ctx, node);
+                break;
+
+            case GGML_OP_GATED_DELTA_NET:
+                ggml_et_op_gated_delta_net(dev_ctx, node);
+                break;
+
+            case GGML_OP_RESHAPE:
+            case GGML_OP_VIEW:
+            case GGML_OP_PERMUTE:
+            case GGML_OP_TRANSPOSE:
+                // These are metadata-only operations that require no computation
+                break;
+
+            default:.
+                break;
+        }*/
+
+        /*switch (op) {
             case GGML_OP_SQR:           ggml_et_op_sqr(env, &node_meta[i]); break;
             case GGML_OP_UNARY:         ggml_et_op_unary(env, &node_meta[i]); break;
             case GGML_OP_SUM_ROWS:      ggml_et_op_sum_rows(env, &node_meta[i]); break;
@@ -523,16 +664,17 @@ int entry_point(struct ggml_cgraph_et* cg, void* env) {
             default:
                 // Log error logic would go here if available on device
                 return -1; 
-        }
-
+        }*/
+                
         // Skip barrier for metadata-only ops and NONE
-        if (op != GGML_OP_RESHAPE &&
-            op != GGML_OP_VIEW    &&
-            op != GGML_OP_PERMUTE &&
-            op != GGML_OP_TRANSPOSE) {
-            device_barrier(32);
+        if (node_op_val != GGML_OP_RESHAPE &&
+            node_op_val != GGML_OP_VIEW    &&
+            node_op_val != GGML_OP_PERMUTE &&
+            node_op_val != GGML_OP_TRANSPOSE) {
+            // device_barrier(32);
+            et_barrier(ET_BARRIER_GLOBAL);
         }
-    }*/
+    }
 
     return 0;
 }
